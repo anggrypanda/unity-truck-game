1a0b0e6d77016a9b6ef92abcbb99e08a89f08db5
AnGGry Panda <madalina.haulica@gmail.com>
2020-09-22 14:14:33 +0300

    Added basic physics and vehicle movement

--- Truck Game/Assets/Scripts/PlayerController.cs
+++ Truck Game/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 15.0f;
+    private float turnSpeed = 25.0f;
+    private float forwardInput;
+    private float horizontalInput; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //We'll move the vehicle forward
+        // Getting the player input
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We move the vehicle sideways
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
